c76107ccecd8e70ce4441eeb3a7cf0d7ab732193
wpbt <bharatthapa45@gmail.com>
2023-06-01 16:36:27 +0545

    CRUD completed

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'is_done'
+    ];
+    
+    protected $casts = [
+        'is_done' => 'boolean'
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
 }
